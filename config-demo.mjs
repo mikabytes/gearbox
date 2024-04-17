ecc0f62d46fe59681bda505f53e4793a10fa332e
mika <1054229+mikabytes@users.noreply.github.com>
2024-04-17 10:49:47 +0200

    Add config-demo file

---
+++ config-demo.mjs
@@ -0,0 +1,8 @@
+export default {
+  backends: [
+    { id: `demo1`,  type: `demo` },
+    { id: `demo2`,  type: `demo` },
+    { id: `demo3`,  type: `demo` },
+    { id: `demo4`,  type: `demo` },
+  ],
+}
