9e3936a1abc04a9dffbdc3b3de7d45adcc9da600
mika <1054229+mikabytes@users.noreply.github.com>
2024-05-01 17:30:31 +0200

    Use full units (MiB) instead of just prefixes (M)

--- public/formatSize.js
+++ public/formatSize.js
@@ -1,18 +1,18 @@
 export default function formatSize(bytes) {
   if (bytes >= 1024 * 1024 * 1024 * 1024) {
-    return `${(bytes / 1024 / 1024 / 1024 / 1024).toFixed(1)} T`
+    return `${(bytes / 1024 / 1024 / 1024 / 1024).toFixed(1)} TiB`
   }
 
   if (bytes >= 1024 * 1024 * 1024) {
-    return `${(bytes / 1024 / 1024 / 1024).toFixed(1)} G`
+    return `${(bytes / 1024 / 1024 / 1024).toFixed(1)} GiB`
   }
 
   if (bytes >= 1024 * 1024) {
-    return `${(bytes / 1024 / 1024).toFixed(1)} M`
+    return `${(bytes / 1024 / 1024).toFixed(1)} MiB`
   }
 
   if (bytes >= 1024) {
-    return `${(bytes / 1024).toFixed(1)} K`
+    return `${(bytes / 1024).toFixed(1)} KiB`
   }
 
   return `${bytes} B`
